0ed7b292614b7e57bd9a1e5ba373be6fdfcbd374
Wataru Haniyama <wataru.haniyama@authlete.com>
2025-04-09 21:49:14 +0900

    refactor: restructure Authlete client creation and API context handling

--- go/main.go
+++ go/main.go
@@ -2,49 +2,31 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
 	"fmt"
 	"os"
 
 	authlete "github.com/authlete/openapi-for-go"
 )
 
-// createAuthleteClient creates and returns an Authlete API client
-func createSimpleClient() (*authlete.APIClient, error) {
+func main() {
 	baseURL := os.Getenv("AUTHLETE_BASE_URL")
 	apiKey := os.Getenv("AUTHLETE_SERVICE_APIKEY")
 	apiSecret := os.Getenv("AUTHLETE_SERVICE_APISECRET")
-	// apiToken  := os.Getenv("AUTHLETE_SERVICEOWNER_ACCESSTOKEN")
+	apiToken := os.Getenv("AUTHLETE_SERVICEOWNER_ACCESSTOKEN")
 	apiVersion := os.Getenv("AUTHLETE_API_VERSION")
 
-	if apiVersion == "V3" {
-		return nil, fmt.Errorf("currently openapi-for-go doesn't support V3 API")
-	}
-
-	//Client for v2.x API
-	cnf := authlete.NewConfiguration()
-	cnf.Servers = authlete.ServerConfigurations{
-		{URL: baseURL},
-	}
-
-	cred := fmt.Sprintf("%s:%s", apiKey, apiSecret)
-	basicCred := "Basic " + base64.StdEncoding.EncodeToString([]byte(cred))
-
-	cnf.DefaultHeader = map[string]string{
-		"Authorization": basicCred,
-	}
-
-	return authlete.NewAPIClient(cnf), nil
-}
-
-func main() {
-	apiClient, err := createSimpleClient()
+	apiClient, err := createSimpleClient(baseURL, apiVersion)
 	if err != nil {
 		fmt.Printf("Error creating Authlete client: %v\n", err)
 		os.Exit(1)
 	}
 
-	ctx := context.Background()
+	ctx, err := createAPIContext(apiKey, apiSecret, apiToken, apiVersion)
+
+	if err != nil {
+		fmt.Printf("Error creating API context: %v\n", err)
+		os.Exit(1)
+	}
 
 	resp, httpResp, err := apiClient.ClientManagementApi.ClientGetListApi(ctx).Execute()
 	if err != nil {
@@ -58,4 +40,33 @@ func main() {
 	for _, client := range resp.Clients {
 		fmt.Printf("Client ID: %d, Client Name: %s\n", client.ClientId, *client.ClientName)
 	}
+}
+
+// createAuthleteClient creates and returns an Authlete API client
+func createSimpleClient(baseURL string, apiVersion string) (*authlete.APIClient, error) {
+
+	if apiVersion == "V3" {
+		return nil, fmt.Errorf("currently openapi-for-go doesn't support V3 API")
+	}
+
+	return createV2Client(baseURL)
+}
+
+func createV2Client(baseURL string) (*authlete.APIClient, error) {
+	//Client for v2.x API
+	cnf := authlete.NewConfiguration()
+	cnf.Servers = authlete.ServerConfigurations{
+		{URL: baseURL},
+	}
+	return authlete.NewAPIClient(cnf), nil
+}
+
+func createAPIContext(apiKey string, apiSecret string, apiToken string, apiVersion string) (context.Context, error) {
+	if apiVersion == "V3" {
+		return nil, fmt.Errorf("currently openapi-for-go doesn't support V3 API")
+	}
+	return context.WithValue(context.Background(), authlete.ContextBasicAuth, authlete.BasicAuth{
+		UserName: apiKey,
+		Password: apiSecret,
+	}), nil
 }
