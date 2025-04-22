a8e754d06ede5163971440563c1d5dd50ab3bb5c
Kerin <kerin.khan@authlete.com>
2025-04-22 15:33:48 -0700

    connects to authlete/openapi-client

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -15,26 +15,25 @@ type RequestArgs = {
   [key: string]: any;
 };
 
-import { Configuration, ClientManagementApi, Middleware } from '@authlete/openapi-client';
+import { Configuration, ClientManagementApi, Middleware, ServiceManagementApi } from '@authlete/openapi-client';
 
 import { ClientAuthorizationGetListApiRequest } from '@authlete/openapi-client';
 
-const req: ClientAuthorizationGetListApiRequest  = {
-  subject: 'kerin'
-};
 
 
 //Get html elements
 
 const basePath = 'https://api.authlete.com';//(document.getElementById('base-url') as HTMLInputElement)?.value || 'https://api.authlete.com';
-const apikey = '353960042211339';//(document.getElementById('api-key') as HTMLInputElement)?.value || process.env.API_KEY;
-const apiSecret = 'C4wvqbJYEq3g5ddbQP_0QsivDq-5FKqY_dvSg6rfoI0'; //(document.getElementById('apiSecret') as HTMLInputElement)?.value || process.env.API_SECRET;
+// service api for service list const apikey = '19568184929257';//(document.getElementById('api-key') as HTMLInputElement)?.value || process.env.API_KEY;
+const apikey = '353960042211339';
+// service list const apiSecret = 'Zlkxn79lxNj8V0GrR6v9xBAQBYy45fc-ezIWkYFHDBo'; //(document.getElementById('apiSecret') as HTMLInputElement)?.value || process.env.API_SECRET;
+const apiSecret = 'C4wvqbJYEq3g5ddbQP_0QsivDq-5FKqY_dvSg6rfoI0';
 const apiVersion = 'v2';//(document.getElementById('apiVersion') as HTMLInputElement)?.value || 'v1';
 
 
 
 function getAuthToken(): string {
-  return '4N34ma206V43XvCJ-FlXBZKyVgplpvEgF02kVi2U9Y8';
+  return 'Xj_-CVjgplKvhu-TVogzbO05tbvXYJxFxwsxWbSjC00';
 }
 
 export class AuthInterceptor extends Configuration {
@@ -46,12 +45,15 @@ export class AuthInterceptor extends Configuration {
         pre(request: RequestArgs): RequestArgs {
           const token = getAuthToken();
           console.log("TOKEN:", token)
+          const authToken = `${apikey}:${apiSecret}`;
+          const authHeader = `Basic ${Buffer.from(authToken).toString('base64')}`;
+          //console.log("Authorization Header:", authHeader);
 
           return {
             ...request,
             headers: {
               ...request.headers,
-              Authorization: `Bearer ${token}`,
+             Authorization: authHeader,
              'X-API-Key': apikey,
              'X-API-Secret': apiSecret,
              'X-API-Version': apiVersion,
@@ -90,14 +92,35 @@ const server = http.createServer((req, res) => {
 
 const api = new ClientManagementApi(AuthInterceptor.Instance);
 
-console.log(api);
+const new_api_service = new ServiceManagementApi(AuthInterceptor.Instance);
 
-api.clientAuthorizationGetListApi(req).subscribe({
-  next: (res) => console.log(res),
-  error: (err) => console.error(err)
-});
+
+//console.log(api);
+
+console.log('Service:::', new_api_service);
+
+
+/*new_api_service.serviceGetListApi({start: 0, end: 10}).subscribe({
+  next: (res) => console.log('V2 Service List:', res),
+  error: (err) => console.error('Error:', err)
+});*/
+
+function req_send() {
+
+  api.clientGetListApi({ developer: 'kerin', start: 0, end: 10 }).subscribe({
+    next: (res) => console.log('Client List:', res),
+    error: (err) => console.error('Error:', err)
+  });
+
+}
+
 
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+//curl -v https://api.authlete.com/api/service/get/list?start=0\&end=5 \
+//-u '19568184929257:Zlkxn79lxNj8V0GrR6v9xBAQBYy45fc-ezIWkYFHDBo'
+
+//curl -v https://api.authlete.com/api/service/get/353960042211339 \
